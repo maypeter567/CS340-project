c2e97eb366bd9ca7c188d521af4ed8e2b0da7ecb
Peter <maypeter0090@gmail.com>
2024-02-27 17:31:44 -0700

    set up Scroller \n\n solving node errors

--- tweeter-web/src/components/Scroller/Scrollers.tsx
+++ tweeter-web/src/components/Scroller/Scrollers.tsx
@@ -1,6 +1,50 @@
+import { useContext } from "react";
+import { UserInfoContext } from "../userInfo/UserInfoProvider";
+import { AuthToken, FakeData, Status, User } from "tweeter-shared";
+import { Link } from "react-router-dom";
+import Post from "../statusItem/Post";
+import useToastListener from "../toaster/ToastListenerHook";
 
+interface Props {
+  value: Status;
+}
 
-const Scroller = () => {
+const Scroller = (props: Props) => {
+  const { displayErrorMessage } = useToastListener();
+  const { setDisplayedUser, currentUser, authToken } = useContext(UserInfoContext);
+
+  const extractAlias = (value: string): string => {
+    let index = value.indexOf("@");
+    return value.substring(index);
+  };
+
+  const getUser = async (
+    authToken: AuthToken,
+    alias: string
+  ): Promise<User | null> => {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.findUserByAlias(alias);
+  };
+
+  const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
+    event.preventDefault();
+
+    try {
+      let alias = extractAlias(event.target.toString());
+
+      let user = await getUser(authToken!, alias);
+
+      if (!!user) {
+        if (currentUser!.equals(user)) {
+          setDisplayedUser(currentUser!);
+        } else {
+          setDisplayedUser(user);
+        }
+      }
+    } catch (error) {
+      displayErrorMessage(`Failed to get user because of exception: ${error}`);
+    }
+  };
 
   return (
     <div className="col bg-light mx-0 px-0">
@@ -8,7 +52,7 @@ const Scroller = () => {
         <div className="row mx-0 px-0">
           <div className="col-auto p-3">
             <img
-              src={item.user.imageUrl}
+              src={props.value.user.imageUrl}
               className="img-fluid"
               width="80"
               alt="Posting user"
@@ -17,19 +61,19 @@ const Scroller = () => {
           <div className="col">
             <h2>
               <b>
-                {item.user.firstName} {item.user.lastName}
+                {props.value.user.firstName} {props.value.user.lastName}
               </b>{" "}
               -{" "}
               <Link
-                to={item.user.alias}
+                to={props.value.user.alias}
                 onClick={(event) => navigateToUser(event)}
               >
-                {item.user.alias}
+                {props.value.user.alias}
               </Link>
             </h2>
-            {item.formattedDate}
+            {props.value.formattedDate}
             <br />
-            <Post status={item} />
+            <Post status={props.value} />
           </div>
         </div>
       </div>
